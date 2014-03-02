5c9a8bc2973fd3ae8de7856814c2989e4b38a393
cyplo <cyplo@cyplo.net>
2014-03-02 09:10:34 +0100

    plotting all voltages

--- plot.py
+++ plot.py
@@ -4,16 +4,18 @@ import datetime
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
+import functools
 
 filepath = "/home/measurements/samples" 
-
-def extract_panel_voltage(sample):
-	return sample['panel_voltage']
 
 def extract_date(sample):
 	timestamp = sample['timestamp']
 	date = datetime.datetime.fromtimestamp(timestamp)
 	return date
+
+def plot(key, dates, all_samples):
+	data = map(lambda sample: sample[key], all_samples)
+	plt.plot(dates, data, label=key)
 
 all_samples = []
 samples_count = 0 
@@ -21,17 +23,26 @@ samples_count = 0
 while samples_count <= 0:
 	try:
 		all_samples = pickle.load( open( filepath, "rb" ) )
-	except(IOError, ValueError):
+	except(IOError, EOFError, ValueError):
 		print "error reading, will retry..."
 		pass
 	samples_count = len(all_samples)
 
 print str(samples_count)+" samples read"		
 
-voltages = map(extract_panel_voltage, all_samples)
+print "extracting data..."
 dates = map(extract_date, all_samples)
+
 print "plotting ..."
-plt.plot(dates, voltages)
-plt.savefig("plot.png")
+dpi=96
+plt.figure(figsize=(1780/dpi, 768/dpi), dpi=dpi)
+
+for key in all_samples[-1].keys():
+	if "voltage" in key or "vcc" in key:
+		plot(key, dates, all_samples)
+
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.8), fancybox=True)
+
+plt.savefig("voltages.png", bbox_inches='tight')
 print "done."
 
