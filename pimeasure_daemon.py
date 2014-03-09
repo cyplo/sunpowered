30a935463517576d54d7c402897ee78c1c2828d7
cyryl plotnicki-chudyk <cyplo@cyplo.net>
2014-03-09 11:40:15 +0100

    daemon now plots

--- pimeasure_daemon.py
+++ pimeasure_daemon.py
@@ -1,5 +1,6 @@
 from measurement_config import filepath
 from measure import measure
+from plot import plot_samples
 import time
 import daemon
 import pickle
@@ -14,15 +15,20 @@ sample_interval = 60 / samples_per_minute
 def main_loop():
     while True:
 	all_samples = []
+	print "loading samples..."
 	try:
    		all_samples = pickle.load( open( filepath(), "rb" ) )
 	except(IOError, EOFError, ValueError):
 		pass
 	samples_count = len(all_samples)
+	print str(samples_count) + " loaded"
 	if(samples_count >= samples_to_keep):
 		all_samples.pop(0)
 	all_samples.append( measure() )
+	print "saving samples..."
 	pickle.dump( all_samples, open( filepath(), "wb" ) )
+	print "plotting saved..."
+	plot_samples(all_samples)
         time.sleep(sample_interval)
 
 main_loop()
--- plot.py
+++ plot.py
@@ -14,56 +14,45 @@ def extract_date(sample):
 	date = datetime.datetime.fromtimestamp(timestamp)
 	return date
 
-def plot(subplot, data, dates):
+def plot(subplot, data, dates, key):
 	subplot.plot(dates, data, label=key)
 
-all_samples = []
-samples_count = 0 
+def plot_samples(all_samples):
 
-while samples_count <= 0:
-	try:
-		all_samples = pickle.load( open( filepath(), "rb" ) )
-	except(IOError, EOFError, ValueError):
-		print "error reading, will retry..."
-		pass
-	samples_count = len(all_samples)
+	print "extracting data..."
+	dates = map(extract_date, all_samples)
 
-print str(samples_count)+" samples read"		
+	print "plotting ..."
+	dpi=96
 
-print "extracting data..."
-dates = map(extract_date, all_samples)
+	fig = plt.figure(figsize=(1920/dpi, 1080/dpi), dpi=dpi)
+	ax = fig.add_subplot(111)
 
-print "plotting ..."
-dpi=96
+	max_voltage = 0
+	min_voltage = 0
 
-fig = plt.figure(figsize=(1920/dpi, 1080/dpi), dpi=dpi)
-ax = fig.add_subplot(111)
+	for key in all_samples[-1].keys():
+		if "voltage" in key or "vcc" in key:
+			data = map(lambda sample: sample[key], all_samples)
 
-max_voltage = 0
-min_voltage = 0
+			local_min_voltage = min(data)		
+			min_voltage = min(min_voltage, local_min_voltage)
 
-for key in all_samples[-1].keys():
-	if "voltage" in key or "vcc" in key:
-		data = map(lambda sample: sample[key], all_samples)
+			local_max_voltage = max(data)		
+			max_voltage = max(max_voltage, local_max_voltage)
 
-		local_min_voltage = min(data)		
-		min_voltage = min(min_voltage, local_min_voltage)
+			plot(ax, data, dates, key)
 
-		local_max_voltage = max(data)		
-		max_voltage = max(max_voltage, local_max_voltage)
+	plt.ylim(min_voltage-0.5, max_voltage+0.5)
 
-		plot(ax, data, dates)
+	print "max voltage: " + str(max_voltage)
+	print "min voltage: " + str(min_voltage)
 
-plt.ylim(min_voltage-0.5, max_voltage+0.5)
+	start, end = ax.get_ylim()
+	plt.yticks(numpy.arange(start, end, 0.5))
+	plt.grid()
+	plt.legend(loc='center left', bbox_to_anchor=(1, 0.7), fancybox=True)
+	plt.savefig("/mnt/measurements/voltages.png", bbox_inches='tight')
 
-print "max voltage: " + str(max_voltage)
-print "min voltage: " + str(min_voltage)
-
-start, end = ax.get_ylim()
-plt.yticks(numpy.arange(start, end, 0.5))
-plt.grid()
-plt.legend(loc='center left', bbox_to_anchor=(1, 0.7), fancybox=True)
-plt.savefig("voltages.png", bbox_inches='tight')
-
-print "done."
+	print "done."
 
