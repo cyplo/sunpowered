b4aebdd93cb54f732dc4867a4989b8f4f912f80d
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2014-07-17 21:59:11 +0200

    on-demand plotting, to be refactored

--- pimeasure_daemon.py
+++ pimeasure_daemon.py
@@ -1,3 +1,4 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from measurement_config import voltages_plot_filepath, samples_filepath, measurement_directory
 from measure import measure
 from plot import plot_samples
@@ -11,6 +12,9 @@ import SocketServer
 import threading
 import resource
 import datetime
+import signal
+import sys
+
 try:
    import cPickle as pickle
 except:
@@ -22,7 +26,7 @@ samples_per_minute = 6
 
 sample_interval = 60 / samples_per_minute
 save_interval = 60 * 15
-plot_interval = 60 * 5
+min_plot_interval = 60
 
 def should_remove_oldest(all_samples):
 	timestamp = all_samples[0]['timestamp']
@@ -35,27 +39,56 @@ def prepare():
     print "preparations done"
     return all_samples
 
-def main_loop():
-    all_samples = prepare()
-    last_saved = time.time()
-    last_plotted = time.time()
-    
-    while True:
-	print "sampling.."
-	all_samples.append( measure() )
+class Sampler:
+	def __init__(self, time_keeper):
+		self.samples = prepare()
+		self.time_keeper = time_keeper
 
-	while(should_remove_oldest(all_samples)):
-		all_samples.pop(0)
+	def sample(self):
+		if(self.time_keeper.can_sample()):
+			print "sampling.."
+			self.samples.append( measure() )
+			while(should_remove_oldest(self.samples)):
+				self.samples.pop(0)
+
+	def save(self, force = False):
+		if( self.time_keeper.can_save() or force):
+			print "saving samples..."
+			pickle.dump( self.samples, open( samples_filepath(), "wb" ) )
+			time_keeper.saved()
+			print str(len(self.samples)) + " samples saved"	
+		
+
+class TimeKeeper:
+    def __init__(self):
+	self.plotted()
+	self.saved()
+
+    def plotted(self):
+	print "plotted"
+	self.last_plotted = time.time()
+	self.plotting = False
+
+    def saved(self):
+	self.last_saved = time.time()
+
+    def can_sample(self):
+	return not self.plotting
+
+    def can_plot(self):
+	result = ((time.time() - self.last_plotted) > min_plot_interval)
+	print "can plot? " + str(result)
+	return result
+
+    def can_save(self):
+	((time.time() - self.last_saved) > save_interval)
 	
-	if( (time.time() - last_saved) > save_interval):
-		print "saving samples..."
-		pickle.dump( all_samples, open( samples_filepath(), "wb" ) )
-		last_saved = time.time()
-		print str(len(all_samples)) + " samples saved"	
+	
+def main_loop(time_keeper, sampler):
+    while True:
+	sampler.sample()
 
-	if((time.time() - last_plotted) > plot_interval):
-		plot_samples(all_samples)
-		last_plotted = time.time()
+	sampler.save()
 
 	last_time = time.time()
 	wait_time = 0
@@ -63,22 +96,35 @@ def main_loop():
 		time.sleep(1)
 		wait_time = time.time() - last_time
 
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+def plot(time_keeper, sampler):
+	if( time_keeper.can_plot() ):
+		time_keeper.plotting = True
+		print "plotting.."
+		plot_samples(sampler.samples)
+		time_keeper.plotted()
+
+		
  
 class GraphServer(BaseHTTPRequestHandler):
+    
     def do_GET(self):
+	print "starting to serve..."
 	self.send_response(200)
 	self.send_header('content-type', 'image/png')
 	self.end_headers()
+	plot(self.time_keeper, self.sampler)
 	f = open(voltages_plot_filepath())
 	self.wfile.write(f.read())
 	f.close()
 	
 
-def serve_plots():
+def serve_plots(time_keeper, sampler):
 	PORT = 8000
 	#Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 	Handler = GraphServer
+	Handler.time_keeper = time_keeper
+	Handler.sampler = sampler
 	httpd = SocketServer.TCPServer(("", PORT), Handler)
 	print "serving at port", PORT
 	os.chdir(measurement_directory())
@@ -96,6 +142,19 @@ def load_samples():
     	print "now " + str(len(all_samples)) + " samples"
 	return all_samples
 
-serve_plots()
-main_loop()
+time_keeper = TimeKeeper() 
+sampler = Sampler(time_keeper)
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+	print "time_keeper" + str(time_keeper)
+	print "sampler" + str(sampler)
+
+	sampler.save(True)
+	plot(time_keeper, sampler)
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+serve_plots(time_keeper, sampler)
+main_loop(time_keeper, sampler)
 
