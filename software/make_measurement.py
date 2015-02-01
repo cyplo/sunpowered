b047510791b09fce282ff29ee77287c9242b37e8
Cyryl Plotnicki-Chudyk <cyplo@cyplo.net>
2015-02-01 15:41:00 +0100

    script for making single measurement and uploading it to influx

--- software/make_measurement.py
+++ software/make_measurement.py
@@ -1,10 +1,35 @@
 from measure import measure
+from influx_config import influx_username, influx_password
 import time
 import pprint
 import json
+import httplib, urllib
+import sys
 
-measurements = measure()
-measurements_json = json.dumps(measurements)
 pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(measurements_json)
+measurements = measure()
+
+series_data = []
+points = []
+points.append([])
+columns = []
+inner_object={}
+for key, value in measurements.iteritems():
+    points[0].append(value)
+    columns.append(key)
+inner_object['points']=points
+inner_object['columns']=columns
+inner_object['name']='balcony'
+series_data.append(inner_object)
+body = json.dumps(series_data)
+
+conn = httplib.HTTPConnection("data.cyplo.net", 8086)
+username=influx_username()
+password=influx_password()
+
+conn.request("POST", "/db/sunpowered/series?u="+username+"&p="+password, body)
+response = conn.getresponse()
+success = (response.status == 200)
+if not success:
+    sys.exit(-1)
 
